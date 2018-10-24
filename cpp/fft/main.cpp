18cfaeeab57814731c8bf26399b236dabf9ba58c
Sam Christy <schristy189@gmail.com>
2018-10-24 19:52:05 -0400

    added gnuplot support

--- cpp/fft/main.cpp
+++ cpp/fft/main.cpp
@@ -1,13 +1,14 @@
-//g++ -lfftw3 -lm main.cpp;./a.out;rm a.out
+//g++ -lfftw3 -lm main.cpp;echo>f1;echo>f2;./a.out;rm a.out;gnuplot plot; rm f1 f2
 
 //TODO
 //vectorize all of these arrays
 
 #include <fftw3.h>
 #include <iostream>
+#include <fstream>
 #include <math.h>
 
-int N=100;
+int N=1000;
 
 int main(){
 	//Allocate the arrays
@@ -20,16 +21,16 @@ int main(){
 	//put the output of the sin function in the input array
 	for(int i=0;i<N;i++)in[i][0]=sin(i);
 
-	//print the input
-	for(int i=0;i<N;i++)std::cout<<in[i][0]<<' ';
-	std::cout<<"\n\n";
+	//save the input
+	std::ofstream f1("f1");
+	for(int i=0;i<N;i++)f1<<in[i][0]<<'\n';
 
 	//Run the transform
 	fftw_execute(p);
 
-	//print the output
-	for(int i=0;i<N;i++)std::cout<<out[i][0]<<' ';
-	std::cout<<"\n\n";
+	//save the output
+	std::ofstream f2("f2");
+	for(int i=0;i<N;i++)f2<<out[i][0]<<'\n';
 
 	//free memory
 	fftw_destroy_plan(p);

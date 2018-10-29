b2ba38313684a131e0b633425ab3324c9aaaa56d
Sam Christy <schristy189@gmail.com>
2018-10-28 20:03:28 -0400

    still doesn't work

--- cpp/arena/arena.cpp
+++ cpp/arena/arena.cpp
@@ -1,24 +1,45 @@
 //g++ arena.cpp; ./a.out; rm a.out
 #include <iostream>
+#include <stdlib.h>
 
 class Fighter{
 public:
 	int health=100;
-	const char* name="Base";
-	volatile int bid(){return 0;}
+	char* name="Base";
+	virtual int bid(){return 0;}
 };
 
 class Bot1: public Fighter{
 public:
-	const char* name="Bot1";
+	char* name="Bot1";
 	int bid(){return 1;}
 };
+
+class Dummy: public Fighter{
+public:
+	char* name="Dummy";
+	int bid(){return 0;}
+};
+
+class Rando: public Fighter{
+public:
+	char* name="Rando";
+	int bid(){return rand()%health-1;}
+};
+
+Fighter *randomFighter(){
+	int choice=rand()%3;
+	switch(choice){
+		case 0: return new Bot1;
+		case 1: return new Dummy;
+		case 2: return new Rando;
+	}
+	return new Bot1();
+}
 
 void fight(Fighter* a,Fighter* b){
 	int bid1=a->bid();
 	int bid2=b->bid();
-	std::cout<<bid1;
-	std::cout<<bid2;
 	if(bid1>bid2){
 		a->health-=bid1;
 		b->health=0;
@@ -29,12 +50,35 @@ void fight(Fighter* a,Fighter* b){
 		a->health-=1;
 		b->health-=1;
 	}
+	std::cout
+<<a->name<<' '
+<<bid1<<' '
+<<a->health<<'\n';
+
+	std::cout
+<<b->name<<' '
+<<bid2<<' '
+<<b->health<<'\n';
 }
 
 int main(){
-	Fighter *f=new Fighter;
-	Bot1 *b=new Bot1;
-	fight(f,b);
-	std::cout<<f->health<<'\n';
-	std::cout<<b->health<<'\n';
+	srand(time(0));
+	Fighter *a=randomFighter();
+	Fighter *b=randomFighter();
+	fight(a,b);
+	std::cout<<randomFighter()->name<<'\n';
+	std::cout<<randomFighter()->name<<'\n';
+	std::cout<<randomFighter()->name<<'\n';
+	std::cout<<randomFighter()->name<<'\n';
+	std::cout<<randomFighter()->name<<'\n';
 }
+
+
+
+
+
+
+
+
+
+

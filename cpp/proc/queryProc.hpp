e130f569681971cff65a33532c53b3996a37c440
Sam Christy <schristy189@gmail.com>
2018-11-14 22:20:39 -0500

    moved the readProc function to this file instead of keeping it in the main file

--- cpp/proc/queryProc.hpp
+++ cpp/proc/queryProc.hpp
@@ -20,3 +20,24 @@ int queryProc(std::string search,bool silent=true){
 	}
 	return pid;
 }
+
+std::vector<std::pair<unsigned long,unsigned long>> readProc(int pid){
+	std::vector<std::pair<unsigned long,unsigned long>> ret;
+	std::string fname="/proc/"+std::to_string(pid)+"/maps";
+	std::string s;
+	std::ifstream f(fname);
+	while(std::getline(f,s)){
+		if(
+			s.find("heap")!=std::string::npos or
+			s.find("stack")!=std::string::npos){
+
+			int dash=s.find("-")+1;
+			unsigned long a,b;
+			a=strtol(s.c_str(),0,16);
+			b=strtol(s.c_str()+dash,0,16);
+			ret.push_back(std::pair<unsigned long, unsigned long>(a,b));
+			std::cout<<s<<'\n';
+		}
+	}
+	return ret;
+}

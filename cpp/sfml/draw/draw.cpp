904e14c93e5e6c62d50df27af7fece9857187653
Sam Christy <schristy189@gmail.com>
2018-11-14 22:21:26 -0500

    working on adding widgets such as buttons and sliders for more user control

--- cpp/sfml/draw/draw.cpp
+++ cpp/sfml/draw/draw.cpp
@@ -15,6 +15,57 @@
  *
  */
 
+class Text{
+public:
+sf::Text text;
+sf::Font font;
+Text(std::string string,int x, int y){
+	font.loadFromFile("/usr/share/fonts/truetype/lato/Lato-Regular.ttf");
+	text.setFont(font);
+	text.setCharacterSize(20);
+	text.setFillColor(sf::Color(20,20,20,255));
+	text.move(x,y);
+	text.setString(string);
+}
+};
+
+class UI{
+public:
+Layer l;
+UI(){
+}
+sf::Sprite getSprite(){
+	l.layer.clear(sf::Color(255,255,255,0));
+	addRibbon();
+	return l.getSprite();
+}
+void addRibbon(){
+	sf::RectangleShape ribbon=sf::RectangleShape(sf::Vector2f(WIDTH,100));
+	ribbon.setFillColor(sf::Color(50,50,50,255));
+	l.layer.draw(ribbon);
+	addButton("Test");
+}
+void addButton(std::string text){
+	int x=10;
+	int y=10;
+
+	Text t=Text(text,x,y);
+	sf::FloatRect bounds=t.text.getGlobalBounds();
+
+	sf::RectangleShape button=sf::RectangleShape(sf::Vector2f(bounds.width,bounds.height));
+	button.setPosition(x,y);
+	button.setFillColor(sf::Color(255,255,255,255));
+
+	l.layer.draw(button);
+	l.layer.draw(t.text);
+
+	//sf::Vector2i mouse=sf::Mouse::getPosition(window);
+	//if(sf::Mouse::isButtonPressed(sf::Mouse::Left)){
+	//	std::cout<<1;
+	//}
+}
+};
+
 class Mouse{
 public:
 sf::Vector2i prev;
@@ -32,6 +83,7 @@ int main(){
 	//create the layers
 	Layer backLayer(299,62,"test.jpg");
 	Layer frontLayer;
+	UI ui();
 
 	//create the pen sprite
 	Pen pen(200,sf::Color(50,50,50,5));
@@ -75,6 +127,7 @@ int main(){
 		//draw the layers
 		window.draw(backLayer.getSprite());
 		window.draw(frontLayer.getSprite());
+		window.draw(ui.getSprite());
 
 		window.display();
 	}

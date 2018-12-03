413986b06631c572653188489cc79ccac4a36f8c
Sam Christy <schristy189@gmail.com>
2018-12-02 23:13:34 -0500

    Now it works with more than just the default audio device

--- cpp/voice recognition/base.cpp
+++ cpp/voice recognition/base.cpp
@@ -1,30 +1,33 @@
 //g++ base.cpp -lasound;./a.out;rm a.out;
-//This program should respond to the user saying a voice command
 
 #include <alsa/asoundlib.h>
 #include <iostream>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 int main(){
 	snd_pcm_t *handle;
 	unsigned char buffer[16*1024];
 
 	//give the buffer data
-	for(int i=0;i<sizeof(buffer);i++)buffer[i]=random()&0xff;
+	//for(int i=0;i<sizeof(buffer);i++)buffer[i]=random()&0xff;
 	for(int i=0;i<sizeof(buffer);i++)buffer[i]=sin(2*i)*0xff;
+	for(int i=0;i<sizeof(buffer);i++)buffer[i]=buffer[i]/10; //attenuate it to 10%
 
-	//opens the audio device
-	snd_pcm_open(&handle,"default",SND_PCM_STREAM_PLAYBACK,0);
+	//Opens the PCM handle. First number is card number second is device
+	//snd_pcm_open(&handle,"default",SND_PCM_STREAM_PLAYBACK,0);
+	snd_pcm_open(&handle,"plughw:1,0",SND_PCM_STREAM_PLAYBACK,0);
 
 	//set parameters of the audio device
 	snd_pcm_set_params(handle,SND_PCM_FORMAT_U8,SND_PCM_ACCESS_RW_INTERLEAVED,1,48000,1,500000);
 
 	//play the audio buffer 10 times
-	for(int i=0;i<10;i++)
+	for(int i=0;i<5;i++)
 		snd_pcm_writei(handle,buffer,sizeof(buffer));
 
 	snd_pcm_close(handle);
 	return 0;
 }
+
+
